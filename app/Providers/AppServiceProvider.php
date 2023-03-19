98b8967688bb1f92d227b47aa2e12b341d0651eb
ahmet.d <ahmet.d@samsungwa.com.tr>
2023-03-19 17:32:42 +0300

    welcome

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Horizon\Horizon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,11 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Horizon::auth(function ($request) {
-            // Always show admin if local development
-            if (env('APP_ENV') == 'local') {
-                return true;
-            }
-        });
+
     }
 }
